3371c6c4242895ac51cf5949ce7c2ab553fcc889
derrickwstout <derrickwstout@gmail.com>
2017-12-12 07:43:49 -0700

    stirngutil test

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
+import(
+     "fmt"
+     "github.com/derrickwstout/stringutil"
+)
 func main() {
+    fmt.Printf(stringutil.Reverse("!desreveR oG ,olleH"))
     fmt.Printf("hello, world From github.com/derrickwstout/hello\n")
     }
